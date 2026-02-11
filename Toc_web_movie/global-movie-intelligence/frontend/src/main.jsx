e35fbeb91cc433523cf33fb3103c40a82fed1dd7
Kukkaiix <66011005@kmitl.ac.th>
2026-02-11 13:39:08 +0700

    fixed index.html

--- Toc_web_movie/global-movie-intelligence/frontend/src/main.jsx
+++ Toc_web_movie/global-movie-intelligence/frontend/src/main.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MovieIntelligence from "./MovieIntelligence";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // Ensure this points to the correct path of your App component
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <MovieIntelligence />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
