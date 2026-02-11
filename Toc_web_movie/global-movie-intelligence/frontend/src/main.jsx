f63ad4c2b34b126f19d629bde44386227dd85059
Kukkaiix <66011005@kmitl.ac.th>
2026-02-11 16:32:12 +0700

    delete for new vite

--- Toc_web_movie/global-movie-intelligence/frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MovieIntelligence from "./MovieIntelligence";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MovieIntelligence />
-  </React.StrictMode>,
-);
