5789d717e7ce1f6e56af80090afda5229bffa860
MayThuKyaw237 <maythu.mtkyaw@gmail.com>
2026-02-23 13:50:42 +0700

    Front appjsx code

--- TOC_MOIVE/src/App.jsx
+++ TOC_MOIVE/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:id" element={<DetailPage />} />
+        <Route path="/movies/:id" element={<DetailPage />} />
         <Route path="/movies" element={<MoviesPage />} />
       </Routes>
     </BrowserRouter>
